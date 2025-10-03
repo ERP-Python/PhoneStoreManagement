537aa80769d1e86f73b25aeade45b670f118b7c4
khanhdepzai206 <nguyenkhanh200603@gmail.com>
2025-10-03 21:50:59 +0700

    fix login

---
+++ backend/khanh.py
@@ -0,0 +1,9 @@
+# khanh.py
+# File test được tạo để commit lịch sử
+# Người thực hiện: Khánh
+
+def hello():
+    print("Xin chào! Đây là file do Khánh tạo để test commit.")
+
+if __name__ == "__main__":
+    hello()
