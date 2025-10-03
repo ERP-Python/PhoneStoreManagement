aece0e5bcb41876a39d4a4cf94622b130a530862
khanhdepzai206 <nguyenkhanh200603@gmail.com>
2025-10-03 22:36:58 +0700

    Delete backend/khanh.py

--- backend/khanh.py
+++
@@ -1,9 +0,0 @@
-# khanh.py
-# File test được tạo để commit lịch sử
-# Người thực hiện: Khánh
-
-def hello():
-    print("Xin chào! Đây là file do Khánh tạo để test commit.")
-
-if __name__ == "__main__":
-    hello()
