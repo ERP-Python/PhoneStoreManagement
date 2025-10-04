bb5105481b59edfa9878497a5de87640dabdcb91
longphu2308 <phulong2308@gmail.com>
2025-10-04 13:51:47 +0700

    Fix bugs

--- backend/apps/catalog/admin.py
+++ backend/apps/catalog/admin.py
@@ -5,11 +5,8 @@ from .models import Brand, Product, ProductVariant, ProductImage, Imei
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'is_active', 'created_at']
-    
     list_filter = ['is_active', 'created_at']
-
     search_fields = ['name', 'slug']
-
     prepopulated_fields = {'slug': ('name',)}
 
 
