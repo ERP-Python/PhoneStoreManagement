47ba035848d92225972e06da98f1296a7c37f6a1
vophuctien3010 <vophuctien3010@gmail.com>
2025-12-03 11:45:35 +0700

    fix/message-2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
         'NAME': os.getenv('DB_NAME', 'phone_store'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', '12345'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Aa123456@'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
